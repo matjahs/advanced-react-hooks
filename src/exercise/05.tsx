7afc31f8490be3583011aaa1be139e53f210a137
Matjah Sonneveld <matjah.sonneveld@blueconic.com>
2022-04-23 09:50:57 +0200

    feat: complete exercise 5

--- src/exercise/05.tsx
+++ src/exercise/05.tsx
@@ -3,23 +3,25 @@
 
 import * as React from "react";
 
-// 🐨 wrap this in a React.forwardRef and accept `ref` as the second argument
-function MessagesDisplay({messages}: any) {
+const MessagesDisplay = React.forwardRef(({messages}: any, ref) => {
   const containerRef = React.useRef<any>();
   React.useLayoutEffect(() => {
     scrollToBottom();
   });
 
-  // 💰 you're gonna want this as part of your imperative methods
-  // function scrollToTop() {
-  //   containerRef.current.scrollTop = 0
-  // }
+  function scrollToTop() {
+    containerRef.current.scrollTop = 0
+  }
   function scrollToBottom() {
     containerRef.current.scrollTop = containerRef.current.scrollHeight;
   }
 
-  // 🐨 call useImperativeHandle here with your ref and a callback function
-  // that returns an object with scrollToTop and scrollToBottom
+  React.useImperativeHandle(ref, () => {
+    return {
+      scrollToTop,
+      scrollToBottom
+    }
+  })
 
   return (
     <div ref={containerRef} role="log">
@@ -31,19 +33,19 @@ function MessagesDisplay({messages}: any) {
       ))}
     </div>
   );
-}
+});
 
 function App() {
   const messageDisplayRef = React.useRef<any>();
   const [messages, setMessages] = React.useState(allMessages.slice(0, 8));
   const addMessage = () =>
     messages.length < allMessages.length
-      ? setMessages(allMessages.slice(0, messages.length + 1))
-      : null;
+    ? setMessages(allMessages.slice(0, messages.length + 1))
+    : null;
   const removeMessage = () =>
     messages.length > 0
-      ? setMessages(allMessages.slice(0, messages.length - 1))
-      : null;
+    ? setMessages(allMessages.slice(0, messages.length - 1))
+    : null;
 
   const scrollToTop = () => messageDisplayRef.current.scrollToTop();
   const scrollToBottom = () => messageDisplayRef.current.scrollToBottom();
